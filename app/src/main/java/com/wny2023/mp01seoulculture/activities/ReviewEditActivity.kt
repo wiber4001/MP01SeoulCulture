dc03a81244a8df641fa25364103481c52b2575b8
wny2023 <wiber4001@gmail.com>
2023-04-26 13:34:18 +0900

    review화면을 구성시작

---
+++ app/src/main/java/com/wny2023/mp01seoulculture/activities/ReviewEditActivity.kt
@@ -0,0 +1,12 @@
+package com.wny2023.mp01seoulculture.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wny2023.mp01seoulculture.R
+
+class ReviewEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_edit)
+    }
+}
--- app/src/main/java/com/wny2023/mp01seoulculture/fragments/ReviewFragment.kt
+++ app/src/main/java/com/wny2023/mp01seoulculture/fragments/ReviewFragment.kt
@@ -1,27 +1,17 @@
 package com.wny2023.mp01seoulculture.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+
 import androidx.fragment.app.Fragment
-import com.wny2023.mp01seoulculture.databinding.FragmentMapBinding
+
 
 
 class ReviewFragment:Fragment() {
 
-    lateinit var binding:FragmentMapBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=FragmentMapBinding.inflate(layoutInflater)
-    }
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return binding.root
+
     }
 
 }
